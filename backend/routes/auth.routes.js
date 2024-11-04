2498a49428faeca840fc90c09c51e7c8a3092a74
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-04 14:32:33 +0300

    half setup the passport js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,22 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/github', async (req, res) => {});
-router.get('/github/callback', async (req, res) => {});
+router.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  }
+);
+router.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: process.env.CLIENT_BASE_URL + '/login',
+  }),
+  function (req, res) {
+    res.redirect(CLIENT_BASE_URL);
+  }
+);
 
 export { router as authRoutes };
