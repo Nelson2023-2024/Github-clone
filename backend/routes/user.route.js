d0fe7410a051bfbc6cf331c93037acc68c1ee50d
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-05 09:24:15 +0300

    Added the like and get likes route in the backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middleware/ensureAuthenticated.js';
+import User from '../models/user.model.js';
 const router = Router();
 
 router.get('/profile/:username', async (req, res) => {
@@ -26,6 +28,52 @@ router.get('/profile/:username', async (req, res) => {
   } catch (error) {
     console.log('Error in getUserProfile Route', error.message);
     res.status(500).json({ error: error.message });
+  }
+});
+
+router.use(ensureAuthenticated);
+
+router.get('/likes', async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id.toString());
+    res.status(200).json({ likedBy: user.likedBy });
+  } catch (error) {
+    console.log('Error in the getLikes controller', error.message);
+    res.status(200).json({ error: 'Internal server error' });
+  }
+});
+router.post('/like/username', async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const authUser = await User.findById(req.user._id.toString());
+    console.log(authUser);
+
+    const userToLike = await User.findOne({ username });
+
+    if (!userToLike)
+      return res
+        .status(404)
+        .json({ error: 'Usernot found / not a member to this app' });
+
+    //if the user is found check if they exist in the authUser array
+    if (authUser.likedProfiles.includes(userToLike.username))
+      return res.status(400).json({ error: 'User already liked' });
+
+    userToLike.likedBy.push({
+      username: authUser.username,
+      avatarUrl: authUser.avatarUrl,
+      likedDate: Date.now(),
+    });
+
+    authUser.likedProfiles.push(userToLike.username);
+
+    await Promise.all([userToLike.save(), authUser.save()]);
+
+    res.status(200).json({ message: 'User Liked successfully' });
+  } catch (error) {
+    console.log('Error in the Liked Controller', error.mesage);
+    res.status(500).json({ error: 'error.message' });
   }
 });
 
